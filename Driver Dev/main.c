8cc71e3c4adaf8c4e195d68e2d00ff806fea8ef8
Shriram M S <shriram.ms@ltts.com>
2021-01-04 09:48:29 +0530

    n

---
+++ Driver Dev/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+  int array[5]={8,9,12,5,1};
+  int n, c, d, swap;
+
+  for (c = 0 ; c < n - 1; c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
+    {
+      if (array[d] > array[d+1]) /* For decreasing order use '<' instead of '>' */
+      {
+        swap       = array[d];
+        array[d]   = array[d+1];
+        array[d+1] = swap;
+      }
+    }
+  }
+
+  printf("Sorted list in ascending order:\n");
+
+  for (c = 0; c < n; c++)
+     printf("%d\n", array[c]);
+
+  return 0;
+}
